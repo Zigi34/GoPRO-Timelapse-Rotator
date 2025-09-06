4ab7d2f57311cc0bb18df988e6881aa380c3e12a
Zdeněk Gold <gol128@vsb.cz>
2025-09-06 20:37:35 +0200

    First version for camera rotator #1
    Configurable duration and speed.

--- time_lapse/time_lapse.ino
+++ time_lapse/time_lapse.ino
@@ -1,41 +1,41 @@
 #include <AccelStepper.h>
 
 /*
- * A camera timelapse rotator 
+ * A camera timelapse rotator
  * using arduino and 28BYJ-48 stepper motor.
- * 
+ *
  * ULN2003A driver setup
  *   + IN1 to D2 (digital pin 2)
  *   + IN2 to D3
  *   + IN3 to D4
  *   + IN4 to D5
  */
+AccelStepper stepper(AccelStepper::HALF4WIRE, 2, 4, 3, 5);
 
-#define SPEED_PIN A0
-#define STEP_NUM 64
-#define MULTIPLIER 5 // change this value to adjust min/max speed
+#define ALL_STEPS       4096
+#define ANGLE           100
+#define ANGLE_STEPS     ALL_STEPS / 360 * ANGLE
+#define DURATION        (60 * 60 * 3.0)
+#define SPEED           ANGLE_STEPS / DURATION
 
-// počet kroků na 120°
-const long steps120 = 4096 / 3;  
-
-AccelStepper stepper = AccelStepper(AccelStepper::HALF4WIRE, 2, 4, 3, 5);
-// notice that two pins(3, 4) are swapped.
+bool forward = true;
 
 void setup() {
-  stepper.setMaxSpeed(1);        // maximum steps after one second
-  stepper.setSpeed(0.1896);      // speed is 120° in 2 hour
-  stepper.moveTo(steps120);      // move to position of 120°
+    stepper.setMaxSpeed(SPEED);         // max. speed (steps/s)
+    stepper.setAcceleration(200);       // acceleration
+    stepper.setCurrentPosition(0);
+
+    stepper.moveTo(ANGLE_STEPS);        // move to position
 }
 
 void loop() {
-  stepper.runSpeedToPosition();  // go to final position
-
-  if (stepper.distanceToGo() == 0) {
-    // change direction
-    if (stepper.currentPosition() > 0) {
-      stepper.moveTo(0);              // backward to start position
-    } else {
-      stepper.moveTo(steps120);       // forward again
+    stepper.run();
+    if (stepper.distanceToGo() == 0) {
+        forward = !forward;
+        if(forward) {
+            stepper.moveTo(ANGLE_STEPS);
+        } else {
+            stepper.moveTo(0);
+        }
     }
-  }
 }
